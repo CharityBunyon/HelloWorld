7d69ce4f6a953e3941f8e7acd261ca90e39ba0b3
Charity Bunyon <CSEYMOU5@GMAIL.com>
2020-03-05 07:36:28 -0600

    completed console input

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -15,22 +15,32 @@ namespace HelloWorld
         {
             Console.WriteLine("Howdy Y'all!!"); 
             Console.ReadKey(); //What happens if you run the app without this line?- If I run the app without this line then the console prints out "Howdy Y'all".
-            var animals = new string[] { "Tricertops", "Gorilla", "Corgi", "Toucan" };
-            foreach (var animal in animals)          
-                {
-                var numOfVowels = 0;
-                foreach (var currentChar in animal)
-                {
-                    if (currentChar == 'a' || currentChar == 'e' || currentChar == 'i' || currentChar == 'o' || currentChar == 'u' || currentChar == 'y')
-                    {
-                        numOfVowels++;
-                    }
-                }
-                if (numOfVowels >= 2)
-                {
-                    Console.WriteLine(animal);
-                }
-            }
+
+            Console.WriteLine("Please enter your name and press enter");
+            var username = Console.ReadLine();
+            Console.WriteLine($"It's nice to meet you {username}.");
+            Console.ReadLine();
+
+            Console.WriteLine($"Hey {username}, what's your favorite color?");
+            var favColor = Console.ReadLine();
+            Console.WriteLine($"Would you like to have a {favColor} triceratops?");
+            Console.ReadLine();
+            //var animals = new string[] { "Tricertops", "Gorilla", "Corgi", "Toucan" };
+            //foreach (var animal in animals)          
+            //    {
+            //    var numOfVowels = 0;
+            //    foreach (var currentChar in animal)
+            //    {
+            //        if (currentChar == 'a' || currentChar == 'e' || currentChar == 'i' || currentChar == 'o' || currentChar == 'u' || currentChar == 'y')
+            //        {
+            //            numOfVowels++;
+            //        }
+            //    }
+            //    if (numOfVowels >= 2)
+            //    {
+            //        Console.WriteLine(animal);
+            //    }
+            //}
         }
     }
 }
